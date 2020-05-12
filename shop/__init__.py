df265b20d2bd96176a57c44def59db5dc63d8d20
Quazix0 <57628611+Quazix0@users.noreply.github.com>
2020-05-12 20:16:21 +0500

    Add files via upload

---
+++ shop/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+import os
+from flask_msearch import Search
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/test.db'
+app.config['SECRET_KEY']='dawdyguihwdjfeo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# из документации https://pythonhosted.org/Flask-Uploads/
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/images')
+
+
+
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+search = Search()
+search.init_app(app)
+
+migrate = Migrate(app, db)
+with app.app_context():
+    if db.engine.url.drivername == "sqlite":
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='customerLogin'
+login_manager.needs_refresh_message_category='danger'
+login_manager.login_message = u"Please login first"
+#импорт маршрутов(в конце файла в связи с защитой от круговой инициализации)
+from shop.products import routes
+from shop.admin import routes
+from shop.carts import carts
+from shop.customers import routes
